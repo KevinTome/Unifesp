190eb0f446136795df0022c63462d71f8eb50774
Kevin Tome <k.tome@unifesp.br>
2022-10-03 15:11:07 -0300

    ProjOO - "Composite"

---
+++ projeto_orientado_a_objeto/composite/exercicio_1/InstituicaoComponent.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public abstract class InstituicaoComponent {
+    public String getAssento() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public List<Individuo> getMembros() {
+        throw new UnsupportedOperationException();
+    }
+    
+}
