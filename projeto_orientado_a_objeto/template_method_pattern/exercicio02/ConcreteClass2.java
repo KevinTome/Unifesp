eb89b46e00114cc432a41b058a277f2c7eba7655
Kevin Tome <k.tome@unifesp.br>
2022-10-22 18:42:53 -0300

    ProjOO - "Template Method Pattern"

---
+++ projeto_orientado_a_objeto/template_method_pattern/exercicio02/ConcreteClass2.java
@@ -0,0 +1,13 @@
+
+public class ConcreteClass2 extends AbstractClass{
+
+	@Override
+	public void metodoPrimitivo1() {
+		System.out.println("classe 2 - método primitivo 1");
+	}
+
+	@Override
+	public void metodoPrimitivo2() {
+		System.out.println("classe 2 - método primitivo 2");
+	}
+}
---
+++ projeto_orientado_a_objeto/template_method_pattern/exercicio03/main.java
@@ -0,0 +1,11 @@
+
+
+public class main {
+
+	public static void main(String[] args){
+
+		// Inicia layout
+		Layout ly = new Layout();
+		
+	}	
+}
