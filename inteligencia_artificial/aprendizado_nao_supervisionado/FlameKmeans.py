114bb700d3c9337b834fceea06b23e7b54682b66
Kevin Tome <k.tome@unifesp.br>
2022-11-10 14:02:21 -0300

    IA - "Aprendizado não supervisionado"

---
+++ inteligencia_artificial/aprendizado_nao_supervisionado/FlameKmeans.py
@@ -0,0 +1,16 @@
+#Flame
+#K-means
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+
+data = pd.read_csv('flame.csv')
+
+X = np.array(data)
+kmeans = KMeans(n_clusters=int(max(data['C'])), random_state = 0).fit(X)
+plt.scatter(X[:,0], X[:,1],c=kmeans.labels_)
+plt.show()
+
+
---
+++ inteligencia_artificial/aprendizado_nao_supervisionado/README.md
@@ -0,0 +1,16 @@
+# Problema: Interpretacao de dados em datasets.
+
+Execute os algoritmos k-means e single linkage (SL) nos datasets a seguir.
+- Aggregation
+- D31
+- Path-based1
+- Flame
+
+Obs: Todas implementacoes foram usadas junto a biblioteca sklearn!
+
+
+
+
+
+
+
