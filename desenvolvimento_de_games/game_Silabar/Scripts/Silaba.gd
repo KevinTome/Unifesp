f6e05336461ff255c7bfbecc7a5700d4443b7420
Kevin Tome <k.tome@unifesp.br>
2022-11-15 18:45:45 -0300

    GameDev - Silabar "Renaming node"

--- desenvolvimento_de_games/game_Silabar/Scripts/Silaba.gd
+++ desenvolvimento_de_games/game_Silabar/Scripts/Silaba.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
 var selected = false
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("click"):
-		selected = true
 		
 func _physics_process(delta):
 	if selected:
@@ -14,3 +10,7 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and not event.pressed:
 			selected = false
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		selected = true
