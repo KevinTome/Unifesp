b5b218aa39fb8624c164c58fba2dcfbe75b6cbd0
Kevin Tome <k.tome@unifesp.br>
2022-11-15 14:05:48 -0300

    GameDev - LJ "Player moving"

---
+++ desenvolvimento_de_games/game_Lazarus_Journey/Scripts/Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+var move_speed = 480
+var gravity = 1200
+var jump_force = -720 #eixo y é negativo para cima
+
+func _physics_process(delta):
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	velocity.y += gravity * delta
+	velocity.x = move_speed * move_direction
+	move_and_slide(velocity) 
+	
+	if Input.is_action_pressed("jump"):
+		velocity.y = jump_force/2
+	
