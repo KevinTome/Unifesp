f27b99252cde618a372138b3c3009b32b0d606e5
Kevin Tome <k.tome@unifesp.br>
2022-11-21 18:41:22 -0300

    GameDev - LJ "Movement correction"

--- desenvolvimento_de_games/game_Lazarus_Journey/Scripts/Player.gd
+++ desenvolvimento_de_games/game_Lazarus_Journey/Scripts/Player.gd
@@ -3,15 +3,31 @@ extends KinematicBody2D
 var velocity = Vector2()
 var move_speed = 480
 var gravity = 1200
-var jump_force = -720 #eixo y é negativo para cima
+var jump_force = -1250 #eixo y é negativo para cima
+var is_grounded
+onready var raycasts = $raycasts
 
 func _physics_process(delta):
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
 	velocity.y += gravity * delta
-	velocity.x = move_speed * move_direction
-	move_and_slide(velocity) 
 	
-	if Input.is_action_pressed("jump"):
+	_get_input()
+	velocity = move_and_slide(velocity)
+	is_grounded = _check_is_grounded() 
+
+		
+func _get_input():
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+	
+	if move_direction != 0:
+		$texture.scale.x *= move_direction
+
+func _input(event):
+	if event.is_action_pressed("jump") and is_grounded:
 		velocity.y = jump_force/2
-	
+		
+func _check_is_grounded():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+	return false
