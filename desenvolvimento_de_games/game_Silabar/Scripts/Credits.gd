24ccb4cf9050b1059a14c4c298a114c950dadbc0
Pedro Dousseau <pv.dousseau@gmail.com>
2022-12-16 02:03:24 -0300

    Create new screens and global script

---
+++ desenvolvimento_de_games/game_Silabar/Scripts/Credits.gd
@@ -0,0 +1,10 @@
+extends Control
+
+onready var audio_player = $AudioPlayer_Override
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	audio_player.play_audio("mus_credit")
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://Scenes/MainScreen.tscn")
